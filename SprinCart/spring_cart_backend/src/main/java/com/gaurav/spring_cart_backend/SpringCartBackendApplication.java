26bd5c01ea202894e9fc2020b919f8c841984f20
Gaurav Bangar <123751854+gauravbangar@users.noreply.github.com>
2024-08-17 09:35:47 +0530

    Add files via upload

---
+++ SprinCart/spring_cart_backend/src/main/java/com/gaurav/spring_cart_backend/SpringCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.spring_cart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCartBackendApplication.class, args);
+	}
+
+}
